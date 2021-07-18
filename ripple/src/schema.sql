d9fa11e489ccc60a4d5a4d3be6b58c9eba6f3cfa
CryptoManiac <balthazar@yandex.ru>
2021-07-18 20:41:49 +0300

    Database schema for ripple-based coins

---
+++ ripple/src/schema.sql
@@ -0,0 +1,66 @@
+BEGIN;
+
+-- Map of temporarity assigned deposit records
+CREATE TABLE IF NOT EXISTS prefix_user_tags(
+    userId BLOB NOT NULL,
+    tag INTEGER NOT NULL
+);
+
+-- Disallow ambiguous user -> tag associations
+CREATE UNIQUE INDEX IF NOT EXISTS prefix_user_tags_userId_uniqualizer ON prefix_user_tags(userId);
+
+-- Disallow ambiguous tag -> user associations
+CREATE UNIQUE INDEX IF NOT EXISTS prefix_awaiting_deposits_amount_uniqualizer ON prefix_user_tags(tag);
+
+-- Transaction log
+CREATE TABLE IF NOT EXISTS prefix_transactions(
+    entryId INTEGER PRIMARY KEY ASC,
+    userId BLOB NOT NULL,
+    amount TEXT NOT NULL,
+    txHash BLOB NOT NULL,
+    blockHash BLOB NOT NULL,
+    blockHeight INTEGER NOT NULL,
+    blockTime INTEGER NOT NULL
+);
+
+-- Disallow duplicate transaction log records
+CREATE UNIQUE INDEX IF NOT EXISTS prefix_transactions_uniquializer ON prefix_transactions(txHash);
+
+CREATE TABLE IF NOT EXISTS prefix_withdrawal_transactions(
+    entryId INTEGER PRIMARY KEY ASC,
+    userId BLOB NOT NULL,
+    amount TEXT NOT NULL,
+    txHash BLOB NOT NULL,
+    blockHash BLOB NOT NULL,
+    blockHeight INTEGER NOT NULL,
+    address TEXT NOT NULL,
+    timestamp INTEGER NOT NULL
+);
+
+-- Disallow duplicate transaction log records
+CREATE UNIQUE INDEX IF NOT EXISTS prefix_withdrawal_transactions_uniquializer ON prefix_withdrawal_transactions(txHash);
+
+-- Pending payments
+CREATE TABLE IF NOT EXISTS prefix_pending(
+    userId BLOB PRIMARY KEY,
+    amount TEXT NOT NULL,
+    address TEXT NOT NULL
+);
+
+-- Accumulated account-specific amounts of fund movement
+CREATE TABLE IF NOT EXISTS prefix_account_stats(
+    userId BLOB PRIMARY KEY ON CONFLICT IGNORE,
+    deposit TEXT NOT NULL,
+    withdrawal TEXT NOT NULL
+);
+
+-- Globally accumulated amounts of fund movement
+CREATE TABLE IF NOT EXISTS prefix_global_stats(
+    deposit TEXT NOT NULL,
+    withdrawal TEXT NOT NULL
+);
+
+-- Create global stats row
+INSERT INTO prefix_global_stats (deposit, withdrawal) SELECT '0', '0' WHERE NOT EXISTS (SELECT * FROM prefix_global_stats);
+
+COMMIT;
